63415ee1dc069c46c919cff50c3334c891630450
JosuCH16 <gjchernandez6@gmail.com>
2025-04-04 22:37:42 -0600

    ahora si 2.0

---
+++ Cartas/src/controlador/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controlador;
+
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public Main() {
+    }
+
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("/vista/CartasVista.fxml"));
+            Pane ventana = (Pane)loader.load();
+            
+            Scene scene = new Scene(ventana);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
